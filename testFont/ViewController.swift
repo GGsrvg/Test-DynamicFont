9fc28e7e9738a57451877c45da4aa63751c4471e
GGsrvg <x@y.z>
2021-02-11 22:23:43 +0300

    init

--- testFont/ViewController.swift
+++ testFont/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let cellName = "cell"
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    let array: [UIFont.TextStyle] = [
+        .largeTitle,
+        .title1,
+        .title2,
+        .title3,
+        .headline,
+        .subheadline,
+        .body,
+        .callout,
+        .footnote,
+        .caption1,
+        .caption2,
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellName)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellName, for: indexPath)
+        let textStyle = array[indexPath.row]
+        cell.textLabel?.text = "\(textStyle.rawValue)"
+        cell.textLabel?.font = UIFont.preferredFont(forTextStyle: textStyle)
+        return cell
+    }
 }
 
